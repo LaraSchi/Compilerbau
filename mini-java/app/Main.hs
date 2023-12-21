3179b5cf9d82e2ff08a8daabfabcfb33c9d7f511
Lara Schierenberg <info@laraschierenberg.de>
2023-12-21 15:08:32 +0100

    todos and file semantics added

--- mini-java/app/Main.hs
+++ mini-java/app/Main.hs
@@ -1,13 +1,16 @@
 module Main (main) where
 
 import Parser (parse)
+import Semantics(checkSemantics)
 
 main :: IO ()
 main = do
     fileContent <- readFile "code/examples/explIfElse.minijava" -- read file
     case parse fileContent of
         Left _  -> putStrLn "Term could not be parsed."
-        Right t -> print t
+        Right t -> case checkSemantics t of
+            Left _   -> putStrLn "Semantic check failed"
+            Right t' -> print t'
 
 {- 
 TODO: 
--- mini-java/src/Semantics.hs
+++ mini-java/src/Semantics.hs
@@ -2,5 +2,17 @@ module Semantics
     ( checkSemantics
     ) where
 
-checkSemantics :: IO ()
-checkSemantics = putStrLn "someFunc"
+import Syntax
+
+checkSemantics :: Program -> Either String Program
+checkSemantics = Right
+
+
+-- #TODO: 
+{- 
+    - rekursive typisierungsfunktion
+    - alle Methoden/Variabeln klar deklariert?
+    - erster Plan: State Monad beim rekursiven Typchecken, alle 
+    Variabeln und Methoden zwischenspeichern zum typchecken?
+    - StateMonad (Menge Typnamen O = {id:ty})
+ -}
