f2c17fd7fb4411a9047f2ea09d633deee246c54a
astammer <anabel.stammer@student.uni-tuebingen.de>
2024-01-15 11:54:59 +0100

    Minor changes

--- mini-java/app/Main.hs
+++ mini-java/app/Main.hs
@@ -8,7 +8,7 @@ import Data.Typeable
 
 main :: IO ()
 main = do
-    fileContent <- readFile "code/examples/explMethod.minijava" -- read file
+    fileContent <- readFile "code/examples/bct.minijava" -- read file
     putStrLn ""
     putStrLn "parsing file content"
     putStrLn ""
