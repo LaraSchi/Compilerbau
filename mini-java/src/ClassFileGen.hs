b1cea7374b12bc56cee342022817e5184727904b
frost <input>
2024-01-16 18:55:34 +0100

    first tries classfile generation

--- mini-java/src/ClassFileGen.hs
+++ mini-java/src/ClassFileGen.hs
@@ -2,9 +2,33 @@ module ClassFileGen where
 
 import Syntax
 import ClassFormat
-import CodeGen
 
 generateClassFile :: Program -> CP_Infos -> ClassFile
-generateClassFile (Program classes typed_bool) CP_Infos = do
+generateClassFile (Program classes typed_bool) cpInfos =
+    let -- Parse the Java class syntax and extract relevant information
+        magicValue = Magic
+        minVer = MinorVersion 0 -- You need to parse the actual version information from the syntax
+        maxVer = MajorVersion 0 -- You need to parse the actual version information from the syntax
+        countCP = length cpInfos
+        arrayCP = buildCPInfosArray cpInfos
+        -- Extract other information from the syntax and populate the fields accordingly
 
-
+    in ClassFile { 
+        magic = magicValue,
+        minver = minVer,
+        maxver = maxVer,
+        count_cp = countCP,
+        array_cp = arrayCP,
+        -- Populate other fields here based on the parsed syntax
+        acfg = undefined,
+        this = undefined,
+        super = undefined,
+        count_interfaces = undefined,
+        array_interfaces = undefined,
+        count_fields = undefined,
+        array_fields = undefined,
+        count_methods = undefined,
+        array_methods = undefined,
+        count_attributes = undefined,
+        array_attributes = undefined
+    }
