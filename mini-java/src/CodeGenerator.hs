f7e9cc4489a837f1218243887fc0c5efd9a7ad0c
frost <fabian.rostomily@student.uni-tuebingen.de>
2024-03-02 15:27:47 +0100

    updated CodeGenerator for new data structure

--- mini-java/src/CodeGenerator.hs
+++ mini-java/src/CodeGenerator.hs
@@ -11,21 +11,20 @@ import Data.List (findIndex)
 
 -- Function to generate assembly code for init method
 generateInitByteCode :: [CP_Info] -> [ByteCodeInstrs]
-generateInitByteCode cp = byteCodeToOpCode_woArgs ALoad_0 ++
-                        (byteCodeToOpCode_w2Args InvokeSpecial 0x00 0x01)
-                        ++ getIndexByDesc ("java/lang/Object" ++ "." ++ "<init>" ++ ":" ++ "()V") cp-- reference to init method
-                        ++ byteCodeToOpCode_woArgs Return
+generateInitByteCode cp = [ALoad_0] ++
+                        [(InvokeSpecial 0x00 (getIndexByDesc ("java/lang/Object" ++ "." ++ "<init>" ++ ":" ++ "()V") cp))] -- reference to init method TODO: what if there are multiple inits
+                        ++ [Return]
 
 -- Function to generate assembly code for a Method
-generateCodeForMethod :: MethodDecl -> [CP_Info] -> [ByteCodeInstrs] -- todo: -> ByteCode_Instrs
+generateCodeForMethod :: MethodDecl -> [CP_Info] -> [ByteCodeInstrs]
 generateCodeForMethod (MethodDecl visibility returnType name params blockStmt) cp_infos =  -- TODO params auf Stack
-    byteCodeToOpCode_woArgs ALoad_0 ++  -- every method starts with this instruction
+    [ALoad_0] ++  -- every method starts with this instruction
     generateCodeForBlockStmt blockStmt cp_infos ++
     if returnType == IntT || returnType == BoolT || returnType == CharT
-        then byteCodeToOpCode_woArgs IReturn
+        then [IReturn]
         else if returnType == VoidT
-            then byteCodeToOpCode_woArgs (Vier Return)
-            else byteCodeToOpCode_woArgs AReturn
+            then [Return]
+            else [AReturn]
 
 -- Function to generate assembly code for BlockStmt
 generateCodeForBlockStmt :: BlockStmt -> [CP_Info] -> [ByteCodeInstrs] -- todo: -> ByteCode_Instrs
@@ -42,9 +41,9 @@ generateCodeForStmt (LocalVarDeclStmt thisType name) = []  -- ist das nur die De
 generateCodeForStmt (LocalVarRefStmt thisType name expr) = 
     if thisType == BoolT
         then if expr == (BoolLitExpr True) 
-            then byteCodeToOpCode_woArgs IConst_1 ++ byteCodeToOpCode_woArgs IStore_1  -- istore nur bei erster variable ... woher weiß ich, wie viele ich schon habe
+            then [IConst_1] ++ [IStore_1]  -- istore nur bei erster variable ... woher weiß ich, wie viele ich schon habe
             else if expr == (BoolLitExpr False) 
-                then byteCodeToOpCode_woArgs IConst_0 ++ byteCodeToOpCode_woArgs IStore_1  -- istore nur bei erster variable ... woher weiß ich, wie viele ich schon habe
+                then [IConst_0] ++ [IStore_1]  -- istore nur bei erster variable ... woher weiß ich, wie viele ich schon habe
                 else [] -- dummy
         else [] --dummy
 generateCodeForStmt (IfStmt expr blockStmt) = generateCodeForExpression expr --Todo Blockstatement?
@@ -105,8 +104,8 @@ generateCodeForExpressions (expr:exprs) =
 ----------------------------------------------------------------------
 -- Helper function for constant pool
 
-getIndexByDesc :: String -> [CP_Info] -> [Int]
+getIndexByDesc :: String -> [CP_Info] -> Int
 getIndexByDesc descriptor cpList =
     case findIndex (\cpInfo -> desc cpInfo == descriptor) cpList of
-        Just idx -> [idx + 1] -- Constant pool begins at 1.
-        Nothing  -> [-1]
+        Just idx -> idx + 1 -- Constant pool begins at 1.
+        Nothing  -> -1
